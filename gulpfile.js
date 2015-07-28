4c71720ef27f39fda7251f9c42c547f812e03732
Mat Scales <mscales@google.com>
2015-07-28 14:58:59 +0100

    Reworked the build process.
    Previously the version built in dist/ didn't actually work. Now, the
    version in src/ will be incomplete but the default gulp task will set
    up watches for all source files, so you can test directly on the dist
    version.
    
    I started removing things from third_party that could come from Bower.
    
    Also simplified things further by removing the JS/Babelify stuff, as
    we are not actually using it right now.
    
    Change-Id: Ie93a7432bc6fae1d10410be2fb12ae2da09f03f6

--- gulpfile.js
+++ gulpfile.js
@@ -15,206 +15,105 @@
  * limitations under the License.
  */
 
-var isProd = false;
-var gulp = require('gulp'),
-    fs = require('fs'),
-    del = require('del'),
-    watch = require('gulp-watch')
-    watchify = require('watchify'),
-    browserify = require('browserify'),
-    source = require('vinyl-source-stream'),
-    gutil = require('gulp-util'),
-    babelify = require('babelify'),
-    minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    streamify = require('gulp-streamify'),
-    runSequence = require('run-sequence'),
-    license = require('gulp-license'),
-    replace = require('gulp-replace'),
-    bump = require('gulp-bump'),
-    vulcanize = require('gulp-vulcanize'),
-    minifyInline = require('gulp-minify-inline'),
-    path = require('path');
+var gulp = require('gulp');
+var bower = require('gulp-bower');
+var del = require('del');
+var license = require('gulp-license');
+var minifycss = require('gulp-minify-css');
+var path = require('path');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var vulcanize = require('gulp-vulcanize');
 
-var version = null;
-
-function createBundle(url) {
-  return browserify({
-    entries: [url],
-    debug: !isProd
-  }).transform(babelify);
-}
-
-function watchBundles() {
-  var bundleKeys = Object.keys(bundles);
-  var watch = null;
-  var key = null;
-  for (var b = 0; b < bundleKeys.length; b++) {
-    key = bundleKeys[b];
-    buildBundle(key);
-    watch = watchify(bundles[key].bundle);
-    watch.on('update', buildBundle.bind(this, key));
+var PATHS = {
+  elements: {
+    src: ['src/elements/**/*'],
+    dest: 'dist/elements'
+  },
+  bower: {
+    src: ['bower.json'],
+    dest: 'dist/bower_components'
+  },
+  static: {
+    src: ['src/*.*'],
+    dest: 'dist'
+  },
+  third_party: {
+    src: ['src/third_party/**/*'],
+    dest: 'dist/third_party'
+  },
+  styles: {
+    src: ['./src/styles/*.scss'],
+    dest: 'dist/styles'
   }
-}
-
-function buildBundle(bundleName) {
-
-  var job = bundles[bundleName];
-  var bundle = job.bundle;
-  var name = job.name;
-  var dest = job.dest || './dist/scripts';
-
-  var b = bundle.bundle()
-      .on('log', gutil.log.bind(gutil, 'Browserify Log'))
-      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-      .pipe(source(name));
-
-  if (isProd) {
-    b = b.pipe(streamify(uglify()));
-  }
-
-  return b.pipe(license('Apache', {
-      organization: 'Google Inc. All rights reserved.'
-    }))
-    .pipe(gulp.dest(dest))
-}
-
-var bundles = {
-  // 'core': {
-  //   url: './src/scripts/app-core.js',
-  //   name: 'app-core.js',
-  //   bundle: null
-  // }
 };
 
 /** Clean */
-gulp.task('clean', function(done) {
-  del(['dist'], done);
+gulp.task('clean:bower', function(done) {
+  del(PATHS.bower.dest, done);
 });
 
-/** Styles */
-gulp.task('styles', function() {
-  return gulp.src('./src/styles/*.scss')
-      .pipe(sass())
-      .pipe(minifycss())
-      .pipe(license('Apache', {
-        organization: 'Google Inc. All rights reserved.'
-      }))
-      .pipe(gulp.dest('./dist/styles'))
+gulp.task('clean:elements', function(done) {
+  del(PATHS.elements.dest, done);
 });
 
-/** Scripts */
-gulp.task('scripts', function() {
-  var bundleKeys = Object.keys(bundles);
-  for (var b = 0; b < bundleKeys.length; b++) {
-    buildBundle(bundleKeys[b]);
-  }
-})
-
-/** Root */
-gulp.task('root', function() {
-  gulp.src('./src/*.*')
-    .pipe(replace(/@VERSION@/g, version))
-    .pipe(gulp.dest('./dist/'));
-
-  return gulp.src('./src/favicon.ico')
-    .pipe(gulp.dest('./dist/'));
+gulp.task('clean:static', function(done) {
+  del(['dist/*.*'], done);
 });
 
-/** HTML */
-gulp.task('html', function() {
-
-  return gulp.src('./src/**/*.html')
-    .pipe(replace(/@VERSION@/g, version))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('clean:third_party', function(done) {
+  del(PATHS.third_party.dest, done);
 });
 
-/** Images */
-gulp.task('images', function() {
-  return gulp.src([
-      './src/**/*.svg',
-      './src/**/*.png',
-      './src/**/*.jpg'])
-    .pipe(gulp.dest('./dist'));
+/** Copy */
+gulp.task('bower', function() {
+  return bower({ cmd: 'update'});
 });
 
-/** Third Party */
+gulp.task('elements', function() {
+  return gulp.src(PATHS.elements.src).pipe(gulp.dest(PATHS.elements.dest));
+});
+
+gulp.task('static', function() {
+  return gulp.src(PATHS.static.src).pipe(gulp.dest(PATHS.static.dest));
+});
+
 gulp.task('third_party', function() {
-  return gulp.src('./src/third_party/**/*.*')
-    .pipe(gulp.dest('./dist/third_party'));
+  return gulp.src(PATHS.third_party.src).pipe(gulp.dest(PATHS.third_party.dest));
 });
 
-/** Service Worker */
-gulp.task('serviceworker', function() {
-  return gulp.src('./src/scripts/sw.js')
-    .pipe(replace(/@VERSION@/g, version))
-    .pipe(gulp.dest('./dist/scripts'));
+/** Build */
+gulp.task('styles', function() {
+ return gulp.src(PATHS.styles.src)
+     .pipe(sass())
+     .pipe(minifycss())
+     .pipe(license('Apache', {
+       organization: 'Google Inc. All rights reserved.'
+     }))
+     .pipe(gulp.dest(PATHS.styles.dest));
 });
 
-/** Vulcanize */
-gulp.task('vulcanize-and-minify', function() {
-
-  return gulp.src('./dist/elements/**/*.html')
-    .pipe(vulcanize({
-      inlineScripts: true,
-      inlineCss: true,
-      stripExcludes: false,
-      excludes: [path.resolve('./dist/third_party/polymer.html')]
-    }))
-    .pipe(minifyInline())
-    .pipe(gulp.dest('./dist/elements'));
-
+gulp.task('vulcanize', function() {
+ return gulp.src('./dist/elements/elements.html')
+   .pipe(vulcanize({
+     inlineScripts: true,
+     inlineCss: true,
+     stripExcludes: false,
+     excludes: [path.resolve('./dist/bower_components/polymer/polymer.html')]
+   }))
+   .pipe(gulp.dest('./dist/elements'));
 });
 
-gulp.task('clean-elements-folder', function(done) {
-  del(['dist/elements/**/*.*',
-      '!dist/elements/**/*.svg',
-      '!dist/elements/**/*.html'], done);
-});
-
-/** Watches */
+/** Watch */
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*.scss', ['styles']);
-  gulp.watch('./src/*.*', ['root']);
-  gulp.watch('./src/**/*.html', ['html']);
-  gulp.watch('./src/images/**/*.*', ['images']);
-  gulp.watch('./src/third_party/**/*.*', ['third_party']);
-  gulp.watch('./src/scripts/sw.js', ['serviceworker']);
-
-  watchBundles();
-});
-
-gulp.task('getversion', function() {
-  version = JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
-});
-
-/** Main tasks */
-
-(function () {
-  var bundleKeys = Object.keys(bundles);
-  var key = null;
-  for (var b = 0; b < bundleKeys.length; b++) {
-    key = bundleKeys[b];
-    bundles[key].bundle = createBundle(bundles[key].url);
-  }
-})();
-
-var allTasks = ['styles', 'scripts', 'root', 'html', 'images',
-    'third_party', 'serviceworker'];
-
-gulp.task('bump', function() {
-  return gulp.src('./package.json')
-    .pipe(bump({type:'patch'}))
-    .pipe(gulp.dest('./'));
+  gulp.watch(PATHS.elements.src, ['elements']);
+  gulp.watch(PATHS.static.src, ['static']);
+  gulp.watch(PATHS.third_party.src, ['third_party']);
+  gulp.watch(PATHS.bower.src, ['bower']);
+  gulp.watch(PATHS.styles.src, ['styles']);
+  gulp.watch([PATHS.elements.dest, PATHS.bower.dest], ['vulcanize']);
 });
 
 gulp.task('default', function() {
-  isProd = true;
-  return runSequence('clean', 'bump', 'getversion', allTasks,
-        'vulcanize-and-minify', 'clean-elements-folder');
-})
-
-gulp.task('dev', function() {
-  return runSequence('clean', 'getversion', allTasks, 'watch');
+  runSequence(['clean:elements', 'clean:third_party', 'clean:static'], ['third_party', 'static', 'elements', 'bower', 'styles'], 'vulcanize', 'watch');
 });
